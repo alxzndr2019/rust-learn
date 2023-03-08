e15dae53cd388a56fac7771b527efe3658e09f00
Ohioze Omiunu Alex <ohiozeomiunu@gmail.com>
2023-03-08 12:49:04 +0100

    first commit

---
+++ src/print.rs
@@ -0,0 +1,13 @@
+pub fn run(){
+    println!("Hello baby");
+
+    //formating
+    println!("Number: {}",1);
+
+    println!("{} is a good {}", "Alex", "Nigga");
+
+    //Positional Arguments
+    println!("{0} is a good {1} and his name is {0}", "Alex", "Nigga");
+
+    
+}
---
+++ src/vars.rs
@@ -0,0 +1,13 @@
+pub fn run(){
+let name = "Alex";
+let job = "Software Engineer";
+print!("My name is {} and i am a {}", name, job);
+
+//Define Constant
+const ID : i32 = 001;
+print!("ID : {}", ID);
+
+//Assign multiple vars
+let (my_name, my_age) = ("Alex", 22);
+print!("{} is {}", my_name, my_age);
+}
